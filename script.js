40da9c60a492d44a8e05e9139bdb06c741ccfb80
csiokos <123376412+csiokos@users.noreply.github.com>
2023-02-25 18:28:36 +0200

    added input for user size, assigned color and black buttons and finally added eventlisteners for mousedown and mouseup to achieve desirable outcome(i used also a mouseDown conditional to ensure that it draws when im holding down the mouse)

--- script.js
+++ script.js
@@ -1,16 +1,65 @@
+const board = document.querySelector(".board");
+const myButton = document.getElementById("myButton");
+let color = 'black';
+
 function createBoard(size){
-  const board = document.querySelector(".board");
+  
 
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
   const numDivs = size*size;
 
+  let isMouseDown = false;
+
   for(let i=0; i<numDivs; i++){
     const div = document.createElement("div");
+    
+    div.addEventListener("mousedown", () => {
+      isMouseDown = true;
+      colorDiv(div)
+    });
+  
+    div.addEventListener("mouseover", () => {
+      if (isMouseDown) {
+        colorDiv(div)
+      }
+    });
+  
+    document.addEventListener("mouseup", () => {
+      isMouseDown = false;
+    });
     board.appendChild(div);
+
   }
 
 }
 
-createBoard(16)
+
+createBoard(16);
+
+myButton.addEventListener('click',()=>{
+  const boardSize = document.getElementById("grid-size-input").value;
+
+  if (boardSize >= 2 && boardSize <= 99) {
+    board.innerHTML = "";
+    createBoard(boardSize);
+  } else {
+    alert("Please enter a number between 2 and 99.");
+  }
+});
+
+function colorDiv(div){
+  if(color === 'random'){
+    div.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`;
+  } else {
+    div.style.backgroundColor = 'black'
+  }
+
+}
+
+function setColor(colorChoice){
+  color = colorChoice;
+
+}
+
